2ea2dfbfb5cc7cb2893f8031402e8d7e70e65e71
LiLiLiLaLa <1921300809@qq.com>
2018-12-13 23:09:53 +0800

    Practice code 我的狗子今天发烧了，希望他睡一觉就好起来^-^

---
+++ P1.java
@@ -0,0 +1,13 @@
+class Person{};
+
+class Student{};
+
+public class P1 {
+    public static void main(String[] args) {
+        fun(new Person());
+        fun(new Student());
+    }
+    public static void fun(Object obj){
+        System.out.println(obj);
+    }
+}
