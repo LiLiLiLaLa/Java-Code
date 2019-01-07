2f052508416585942a79e8d5c88a0088d81a43c5
LiLiLiLaLa <1921300809@qq.com>
2019-01-07 21:22:28 +0800

    Proxcy

---
+++ 静态代理模式.java
@@ -0,0 +1,29 @@
+public interface BuyHouse {
+    void buyHouse();
+}
+
+class BuyHouseImpl implements BuyHouse {
+    @Override
+    public void buyHouse() {
+        System.out.println("我要买房");
+    }
+}
+
+/**
+ * 这是买房子的代理类
+ */
+class BuyHouseProxy implements BuyHouse{
+    private BuyHouse buyHouse;
+
+
+    public BuyHouseProxy(BuyHouse buyHouse){
+        this.buyHouse = buyHouse;
+    }
+
+    @Override
+    public void buyHouse() {
+        System.out.println("买房前准备");
+        buyHouse.buyHouse();//真的买的步骤还是由客户来完成
+        System.out.println("买房后装修");
+    }
+}
