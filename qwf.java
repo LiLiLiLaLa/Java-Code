04954959f2ba8abd4bb1fef5ae879b8bcddeebc4
LiLiLiLaLa <1921300809@qq.com>
2019-01-18 13:41:26 +0800

    add some files

---
+++ qwf.java
@@ -0,0 +1,36 @@
+//import java.io.File;
+//import java.io.FileInputStream;
+//import java.io.InputStream;
+//
+//public class Test{
+//    public static void main(String[] args) {
+//        int age = 18;
+//        String name = "邹狗子";
+//        Dog dog = new Dog(name,age);
+//        int x = age - 1;
+//        if(x > 10){
+//            dog.play();
+//        }else{
+//            dog.bark();
+//        }
+//        int[] numList = new int[]{1,2,3,4};
+//        //这是一段注释
+//
+//        //JDK8新增的lambda表达式
+//        //new Thread(()-> System.out.println("Hello dog")).start();
+//    }
+//}
+//class Dog{
+//    private String name;
+//    private Integer age;
+//    public Dog(String name,Integer age){
+//        this.name = name;
+//        this.age = age;
+//    }
+//    public void play(){
+//        System.out.println(this.name + "和我一起玩");
+//    }
+//    public void bark(){
+//        System.out.println(this.name + "汪汪叫");
+//    }
+//}
