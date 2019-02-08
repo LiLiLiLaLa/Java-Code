baa21c1bfdda75ed14267b96e0bfdbff0d36586e
LiLiLiLaLa <1921300809@qq.com>
2019-02-08 20:57:43 +0800

    practice code

--- C.java
+++ C.java
@@ -50,3 +50,167 @@ public class C{
         temp.append("\n").append("www.zoudapaochibaba.cn");
     }
 }
+
+//class Person{
+//    private String name;
+//    private int age;
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//}
+//
+//class Student{
+//    private String name;
+//    private int age;
+//    private String school;
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
+//
+//    public void setSchool(String school) {
+//        this.school = school;
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public String getSchool() {
+//        return school;
+//    }
+//}
+
+//class Person{
+//    private String name;
+//    private int age;
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//}
+//
+////定义一个子类
+//class Student extends Person{
+//    private String school;
+//
+//    public String getSchool() {
+//        return school;
+//    }
+//
+//    public void setSchool(String school) {
+//        this.school = school;
+//    }
+//}
+//
+//public class Test{
+//    public static void main(String[] args){
+//        Student stu = new Student();
+//        stu.setName("Steven");
+//        stu.setAge(18);
+//        stu.setSchool("高新一中");
+//        System.out.println("姓名：" + stu.getName() + "，年龄：" + stu.getAge() + "，学校是：" + stu.getSchool());
+//
+//    }
+//}
+////姓名：Steven，年龄：18，学校是：高新一中
+
+//class Person{
+//    public Person(){
+//        System.out.println("父类对象产生");
+//    }
+//}
+//class Student extends Person{
+//    public Student(){
+//        super() ; //此语句在无参时写于不写一样
+//        System.out.println("子类对象产生");
+//    }
+//}
+//public class Test{
+//    public static void main(String[] args) {
+//        new Student();
+//    }
+//}
+////父类对象产生
+////子类对象产生
+
+//class Person{
+//    private String name;
+//
+//    public String getName(){
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//}
+//
+//class Student extends Person{
+//    public void fun(){
+//        System.out.println(getName());
+//    }
+//}
+//
+//public class Test{
+//    public static void main(String[] args) {
+//        Student stu  = new Student();
+//        stu.setName("Steven");
+//        System.out.println(stu.getName());
+//        stu.fun();
+//    }
+//}
+////Steven
+////Steven
+
+class Person{
+    public void print(){
+        System.out.println("1.Person类的print方法");
+    }
+}
+
+class Student extends Person{
+    public void print(){
+        System.out.println("2.Student类的print方法");
+    }
+}
+
+public class Test{
+    public static void main(String[] args) {
+        new Student().print();
+    }
+}
+//2.Student类的print方法
