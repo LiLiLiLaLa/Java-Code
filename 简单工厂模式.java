42b004f497f29793dee3ef231387db396afd3e59
LiLiLiLaLa <1921300809@qq.com>
2019-02-18 23:34:36 +0800

    review notes

--- 简单工厂模式.java
+++ 简单工厂模式.java
@@ -37,3 +37,133 @@ public class Demo{
 		ret.fun();
 	}
 }
+
+//interface Computer{
+//    void printComputer();
+//}
+//
+//class MackbookProComputer implements Computer{
+//    @Override
+//    public void printComputer() {
+//        System.out.println("MacbookPro");
+//    }
+//}
+//
+//class SurfaceBookComputer implements Computer{
+//    @Override
+//    public void printComputer() {
+//        System.out.println("SurfaceBook");
+//    }
+//}
+//
+//interface ComputerFactory{
+//    Computer createComputer();
+//}
+//
+//class AppleFactory implements ComputerFactory{
+//    @Override
+//    public Computer createComputer() {
+//        return new MackbookProComputer();
+//    }
+//}
+//
+//class MsFactory implements ComputerFactory{
+//    @Override
+//    public Computer createComputer() {
+//        return new SurfaceBookComputer();
+//    }
+//}
+//
+//public class Client{
+//    public void buyComputer(Computer computer){
+//        computer.printComputer();;
+//    }
+//
+//    public static void main(String[] args) {
+//        Client c = new Client();
+//        ComputerFactory f = new AppleFactory();
+//        c.buyComputer(f.createComputer());
+//    }
+//}
+
+interface Computer{
+    void printComputer();
+}
+
+class MacbookProComputer implements Computer{
+    @Override
+    public void printComputer() {
+        System.out.println("MackbookPro");
+    }
+}
+
+class SurfaceBookComputer implements Computer{
+    @Override
+    public void printComputer() {
+        System.out.println("SurfeceBook");
+    }
+}
+
+interface OperatingSystem{
+    void printSystem();
+}
+
+class MacOsSystem implements OperatingSystem{
+    @Override
+    public void printSystem() {
+        System.out.println("This is mac os");
+    }
+}
+
+class Windows8System implements OperatingSystem{
+    @Override
+    public void printSystem() {
+        System.out.println("This is window8");
+    }
+}
+
+interface ProdectionFactory{
+    Computer createComputer();
+    OperatingSystem createSystem();
+}
+
+class AppleFactory implements ProdectionFactory{
+    @Override
+    public Computer createComputer() {
+        return new MacbookProComputer();
+    }
+
+    @Override
+    public OperatingSystem createSystem() {
+        return new MacOsSystem();
+    }
+}
+
+class MsFactory implements ProdectionFactory{
+    @Override
+    public Computer createComputer() {
+        return new MacbookProComputer();
+    }
+
+    @Override
+    public OperatingSystem createSystem() {
+        return new MacOsSystem();
+    }
+}
+
+public class Client{
+    public void buyComputer(Computer computer){
+        computer.printComputer();
+    }
+    public void use(OperatingSystem s){
+        s.printSystem();
+    }
+    public static void main(String[] args) {
+        Client c = new Client();
+        ProdectionFactory factory = new AppleFactory();
+        Computer computer = factory.createComputer();
+        OperatingSystem system = factory.createSystem();
+        c.buyComputer(computer);
+        c.use(system);
+    }
+}
