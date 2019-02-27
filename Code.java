7228d7d7f5000b63dd89fdec5b0afd2ba817fc06
LiLiLiLaLa <1921300809@qq.com>
2019-02-27 22:34:47 +0800

    practice code

--- Code.java
+++ Code.java
@@ -63,3 +63,41 @@ public class Message {
         System.out.println("[Message] Hello Package");
     }
 }
+
+//class Singleton{
+//    public void print(){
+//        System.out.println("Hello world");
+//    }
+//}
+//
+//public class SingletonTest {
+//    public static void main(String[] args) {
+//        Singleton singleton = null;//声明对象
+//        singleton = new Singleton();//实例化对象
+//        singleton.print();
+//    }
+//}
+
+//class Singleton{
+//    private Singleton(){}//private声明构造
+//    public void print(){
+//        System.out.println("Hello world");
+//    }
+//}
+
+class Singleton{
+    //在类的内部可以访问私有结构，所以可以在类的内部产生实例化对象
+    static Singleton instance = new Singleton();
+    private Singleton(){}//private声明构造方法
+    public void print(){
+        System.out.println("我是小可爱");
+    }
+}
+public class SingletonTest{
+    public static void main(String[] args) {
+        Singleton singleton = null;//声明对象
+        singleton = Singleton.instance;
+        singleton.print();
+    }
+}
+//我是小可爱
