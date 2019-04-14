771e65b4c6a3a72860d8718d0f62d25dd8815667
LiLiLiLaLa <1921300809@qq.com>
2019-04-14 21:32:11 +0800

    practice code

--- P(2).java
+++ P(2).java
@@ -38,3 +38,158 @@ public class P1 {
 
 
 }
+
+//public class Test {
+//    public static void main(String[] args) {
+//        System.out.println(Color.getInstance(Color.BLUE_FLAG ));
+//    }
+//}
+//
+//class Color {
+//    private String title;
+//    public static final int RED_FLAG = 1;
+//    public static final int GREEN_FLAG = 2;
+//    public static final int BLUE_FLAG = 3;
+//    private static final Color RED = new Color("RED");
+//    private static final Color GREEN = new Color("GREEN");
+//    private static final Color BLUE = new Color("BLUE");
+//    public Color(String title) {
+//        this.title = title;
+//    }
+//    public static Color getInstance(int ch) {
+//        switch(ch){
+//            case RED_FLAG:
+//                return RED;
+//            case GREEN_FLAG:
+//                return GREEN;
+//            case BLUE_FLAG:
+//                return BLUE;
+//            default:
+//                return null;
+//        }
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return this.title;
+//    }
+//}
+//enum Color {
+//    RED,BLUE,GREEN
+//}
+//
+//public class Test {
+//    public static void main(String[] args) {
+//        //System.out.println(Color.BLUE.ordinal() + "=" + Color.BLUE.name());
+//        for (Color temp:Color.values()) {
+//            System.out.println(temp.ordinal() + "=" + temp.name());
+//        }
+//    }
+//}
+////0=RED
+////1=BLUE
+////2=GREEN
+
+//enum Color {
+//    RED("红色"),GREEN("绿色"),BLUE("蓝色");
+//    private String title;
+//    Color(String color) {
+//        this.title = color;
+//    }
+//    @Override
+//    public String toString() {
+//        return this.title;
+//    }
+//}
+//
+//public class Test{
+//    public static void main(String[] args) {
+//        System.out.println(Color.BLUE);
+//    }
+//}
+////蓝色
+
+////枚举类实现接口
+//interface IColor {
+//    public String getColor();
+//}
+//enum Color implements IColor {
+//    RED("红色"),GREEN("绿色"),BLUR("蓝色");
+//    private String title;
+//    //构造方法私有化
+//    private Color(String title) {
+//        this.title = title;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return this.title;
+//    }
+//
+//    @Override
+//    public String getColor() {
+//        return this.title;
+//    }
+//}
+//
+//public class Test {
+//    public static void main(String[] args) {
+//        IColor iColor = Color.RED;
+//        System.out.println(iColor.getColor());
+//    }
+//}
+////红色
+
+//enum Sex {
+//    MALE("男"),FEMALE("女");
+//    private String title;
+//    private Sex(String title) {
+//        this.title = title;
+//    }
+//    @Override
+//    public String toString() {
+//        return this.title;
+//    }
+//}
+//
+//class Person {
+//    private String name;
+//    private int age;
+//    private Sex sex;
+//    public Person(String name, int age, Sex sex){
+//        this.name = name;
+//        this.age = age;
+//        this.sex = sex;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Person [name = " + name + ",age = " + age + ",sex = " + sex + "]";
+//    }
+//}
+//
+//public class Test {
+//    public static void main(String[] args) {
+//        Person per = new Person("邹鸡儿", 20, Sex.MALE);
+//        System.out.println(per);
+//    }
+//}
+////Person [name = 邹鸡儿,age = 20,sex = 男]
+
+enum Sex {
+    MALE, FEMALE
+}
+
+public class Test {
+    public static void main(String[] args) {
+        switch(Sex.MALE){
+            case MALE :
+                System.out.println("男");
+                break;
+            case FEMALE :
+                System.out.println("女");
+                break;
+        }
+    }
+}
+//男
