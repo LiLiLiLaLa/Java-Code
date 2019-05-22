7e30e69d08c894dc1bb9f2d4af9c9f40fa0abc9d
LiLiLiLaLa <1921300809@qq.com>
2019-05-22 21:07:39 +0800

    response

--- untitled/src/xpu/edu/web/servlet/Cookie/CookieDemo1.java
+++ untitled/src/xpu/edu/web/servlet/Cookie/CookieDemo1.java
@@ -1,28 +1,23 @@
-package xpu.edu.web.servlet.request;
+package xpu.edu.web.servlet.Cookie;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Enumeration;
-import java.util.Map;
-import java.util.Set;
 
-@WebServlet("/RequestDemo7")
-public abstract class RequestDemo7 extends HttpServlet {
+@WebServlet("/CookieDemo1")
+public class CookieDemo1 extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //设置流的编码
-        request.setCharacterEncoding("UTF-8");
-
-        //获取请求参数username
-        String username = request.getParameter("username");
-        System.out.println(username);
-
+        //创建Cookie对象
+        Cookie c = new Cookie("msg","hello");
+        //发送Cookie
+        response.addCookie(c);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        this.doPost(request,response);
+        this.doPost(request, response);
     }
 }
